8ab80d36e97593eb2bedddbb7ba066fed9cecf29
Leo Mihalcea <leomihalcea@gmail.com>
2026-02-03 21:34:28 +0200

    feat: replace vue frontend with angular

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,27 +1,6 @@
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
-import PrimeVue from 'primevue/config';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-import App from './App.vue'
-import router from './router'
-import {Button} from 'primevue';
-
-import './style.css'
-import {DefaultTheme} from "@/theme/default.ts";
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-app.use(PrimeVue, {
-    theme: {
-        preset: DefaultTheme,
-        options: {
-            darkModeSelector: false
-        }
-    }
-})
-
-app.component('Button', Button);
-
-app.mount('#app')
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
